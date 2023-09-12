f174b4e0a24c14315e634518783c4366ad9f94e8
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2023-09-12 12:48:37 +0200

    ✨ Mazes:  many developments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,13 @@
 import { defineConfig } from "astro/config";
-import remarkToc from "remark-toc";
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
   base: "",
   site: "http://localhost:3000",
-  experimental: {
-    assets: true,
-    viewTransitions: true,
-  },
   integrations: [
     mdx({
       syntaxHighlight: "shiki",
       shikiConfig: { theme: "github-dark" },
-      // Links don't work as expected
-      // remarkPlugins: [[remarkToc, { heading: "Table des matières" }]],
-      //
     }),
   ],
 });
